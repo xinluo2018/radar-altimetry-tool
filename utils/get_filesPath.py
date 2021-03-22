8bbbed537dfc2c85e9c1a3df6a7077f0318c762b
xin luo <xinluo_xin@163.com>
2021-03-22 22:53:29 +0800

    new commit

--- utils/get_filesPath.py
+++ utils/get_filesPath.py
@@ -1,22 +1,5 @@
 import os
 
-# def get_filesPath(dir, l2_product = 'enhanced_measurement.nc'):
-#     '''
-#     get the files name corresponding to the giving directory
-#     input: 
-#         dir: str, files directory
-#         l2_product: product stype, e.g., enhanced_measurement.nc, 
-#         standard_measurement.nc...
-#     ouput:
-#         f_names_filter: list, contains all the file names (string).
-#     '''
-#     f_paths = os.listdir(dir)
-#     f_paths_filter = []
-#     for i in f_paths:
-#         if os.path.splitext(i)[1] == '.SEN3':
-#             path = os.path.join(dir, i, l2_product)
-#             f_paths_filter.append(path)
-#     return f_paths_filter
 
 def get_filesPath(base, key_words):    
     '''
